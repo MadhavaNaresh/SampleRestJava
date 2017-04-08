bb1017800ee6b32a0ff225a1176bad995dd549a3
MadhavaNaresh <madhav.naresh@live.com>
2017-04-08 13:03:31 +0530

    Initial branch commit
    Adding business logic

--- src/main/java/Business/CoordinatesInfo.java
+++ src/main/java/Business/CoordinatesInfo.java
@@ -4,4 +4,6 @@ package Business;
  * Created by MS030628 on 4/8/2017.
  */
 public interface CoordinatesInfo {
+    long Latitude = 0;
+    long Longitude = 0;
 }
--- src/main/java/Business/WeatherInfo.java
+++ src/main/java/Business/WeatherInfo.java
@@ -3,5 +3,7 @@ package Business;
 /**
  * Created by MS030628 on 4/8/2017.
  */
-public interface WeatherInfo {
+public interface WeatherInfo extends CoordinatesInfo {
+    String GetWeatherByLocation(String location_name);
+    String GetWeatherByCoordinates(CoordinatesInfo obj);
 }
