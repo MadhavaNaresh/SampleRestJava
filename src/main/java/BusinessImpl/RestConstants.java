74effdae714ef5b44f0e57d592bcea42497cc804
MadhavaNaresh <madhav.naresh@live.com>
2017-04-08 15:56:22 +0530

    CI build

---
+++ src/main/java/BusinessImpl/RestConstants.java
@@ -0,0 +1,8 @@
+package BusinessImpl;
+
+/**
+ * Created by MS030628 on 4/8/2017.
+ */
+public class RestConstants {
+    public static final String REST_URI = "api.openweathermap.org/data/2.5/weather?q=";
+}
--- src/main/java/BusinessImpl/WeatherInfoImpl.java
+++ src/main/java/BusinessImpl/WeatherInfoImpl.java
@@ -5,7 +5,9 @@ import Business.WeatherInfo;
  * Created by MS030628 on 4/8/2017.
  */
 public class WeatherInfoImpl implements WeatherInfo {
-    public String GetWeatherByLocation(String location_name) {
+    public String GetWeatherByLocation(String location_name)
+    {
+        String REST_URL = RestConstants.REST_URI.concat(location_name);
         return null;
     }
 
