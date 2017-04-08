85d61a9acac0bcb3f1ccc3e24bb69a92b040fd33
MadhavaNaresh <madhav.naresh@live.com>
2017-04-08 13:13:57 +0530

    Few more changes
    Removed default classes

---
+++ src/main/java/BusinessImpl/AppStart.java
@@ -0,0 +1,11 @@
+package BusinessImpl;
+
+/**
+ * Created by MS030628 on 4/8/2017.
+ */
+public class AppStart {
+
+    public static void main(String[] args){
+        System.out.println("This program retrieves weather information");
+    }
+}
---
+++ src/main/java/BusinessImpl/WeatherInfoImpl.java
@@ -0,0 +1,15 @@
+package BusinessImpl;
+import Business.CoordinatesInfo;
+import Business.WeatherInfo;
+/**
+ * Created by MS030628 on 4/8/2017.
+ */
+public class WeatherInfoImpl implements WeatherInfo {
+    public String GetWeatherByLocation(String location_name) {
+        return null;
+    }
+
+    public String GetWeatherByCoordinates(CoordinatesInfo obj) {
+        return null;
+    }
+}
